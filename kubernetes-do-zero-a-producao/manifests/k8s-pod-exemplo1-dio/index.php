9919649273b2e461fbefb04d5e1dd9268f8bbbe8
Rodrigo de Souza <rodrigo.souza@avare.sp.gov.br>
2025-04-11 18:30:04 -0300

    Add Kubernetes manifests for myapp-php deployment, service, and configuration

---
+++ kubernetes-do-zero-a-producao/manifests/k8s-pod-exemplo1-dio/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+ <meta charset="UTF-8">
+ <head>
+  <title>MyApp PHP 1.0</title>
+ </head>
+ <body>
+    <h1>MyApp PHP 1.0</h1>
+ <?php
+
+ echo gethostname();
+ echo "<br>";
+ echo $_SERVER["REMOTE_ADDR"];
+ echo "<br>";
+ echo date('Y-m-d H:i:s');
+ 
+ ?>
+ </body>
+</html>
